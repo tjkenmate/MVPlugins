24705191ad001446c6388ccef930b1818b7cab5c
TjKenMate <tjkenmate@gmail.com>
2017-08-19 22:07:28 -0400

    More Dashing

--- TJ_Dash_Overhaul.js
+++ TJ_Dash_Overhaul.js
@@ -8,10 +8,19 @@
  * @desc if true this will disable The Dash Menu option
  * Default: true
  * @default true
+ * 
+ * @param ~~ Defaults ~~
+ * @default 
+ * 
+ * @param Default Move Speed
+ * @desc The Default Movment Speed of the party before modifiers
+ * (4 is the default speed of the system)
+ * Default: 4 
+ * @default true
  */
 
 var Imported = Imported || {};
-Imported.TjKenMate_KMSStatusGauges = true;
+Imported.TjKenMate_DashOverhaul = true;
 
 var TjKenMate = TjKenMate || {};
 TjKenMate.DashOverhaul = TjKenMate.DashOverhaul || {};
@@ -21,7 +30,17 @@ TjKenMate.Parameters = PluginManager.parameters('TJ_Dash_Overhaul');
 
 TjKenMate.Param = TjKenMate.Param || {};
 
-TjKenMate.Param.DisableAlwaysDashOption = eval(TjKenMate.Parameters['Disable AlwaysDash']);
+TjKenMate.Param.DisableAlwaysDashOption = Boolean(TjKenMate.Parameters['Disable AlwaysDash']);
+
+TjKenMate.Param.DashOverhaul.DefaultSettings = TjKenMate.DashOverhaul.DefaultSettings || {};
+Tjkenmate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed = Number(TjKenMate.Parameters['Default Move Speed'] || 4)
+
+var _Tj_DashOverhaul_Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
+
+Game_CharacterBase.prototype.initMembers = function() {
+    _Tj_DashOverhaul_Game_CharacterBase_initMembers.call(this);
+    this.setMoveSpeed(TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed);
+};
 
 if (TjKenMate.Param.DisableAlwaysDashOption){
     if(TjKenMate.OptionMenu){}
@@ -29,6 +48,7 @@ if (TjKenMate.Param.DisableAlwaysDashOption){
         Window_Options.prototype.addGeneralOptions = function() {
             //this.addCommand(TextManager.alwaysDash, 'alwaysDash');
             this.addCommand(TextManager.commandRemember, 'commandRemember');
+            console.log('This was called and the value is ' + TjKenMate.DisableAlwaysDashOption);
         };
     }
-}
+};
