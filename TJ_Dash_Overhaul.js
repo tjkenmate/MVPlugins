73cb21d824f22f40cd2a570263f0a7dfa7892027
TjKenMate <tjkenmate@gmail.com>
2017-08-19 23:50:07 -0400

    DONE FOR THE NIGHT

--- TJ_Dash_Overhaul.js
+++ TJ_Dash_Overhaul.js
@@ -15,8 +15,24 @@
  * @param Default Move Speed
  * @desc The Default Movment Speed of the party before modifiers
  * (4 is the default speed of the system)
- * Default: 20 
- * @default 20
+ * Default: 4 
+ * @default 4
+ * 
+ * @param Default Dash Speed
+ * @desc The Default Dash Speed of the party before modifiers
+ * (5 is the default speed of the system)
+ * Default: 6 
+ * @default 6
+ * 
+ * @param Default Max Stamina
+ * @desc The Default Max Stamina of the party before modifiers
+ * Default: 100
+ * @default 100
+ * 
+ * @param Default Stamina counter gain
+ * @desc The Default Stamina counter gain of the party (See stamina gain)
+ * Default: 5
+ * @default 5
  */
 
 var Imported = Imported || {};
@@ -30,20 +46,63 @@ TjKenMate.Parameters = PluginManager.parameters('TJ_Dash_Overhaul');
 
 TjKenMate.Param = TjKenMate.Param || {};
 
-TjKenMate.Param.DisableAlwaysDashOption = Boolean(TjKenMate.Parameters['Disable AlwaysDash']);
-//TjKenMate.Param.DashOverhaul = TjKenMate.Param.DashOverhaul || {};
-TjKenMate.Param.DashOverhaul.DefaultSettings = TjKenMate.DashOverhaul.DefaultSettings || {};
-TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed = Number(TjKenMate.Parameters['Default Move Speed'] || 4);
+TjKenMate.Param.DashOverhaul = TjKenMate.Param.DashOverhaul || {};
+TjKenMate.Param.DashOverhaul.DisableAlwaysDashOption = Boolean(TjKenMate.Parameters['Disable AlwaysDash']);
 
-var _Tj_Dash_Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
-Game_CharacterBase.prototype.initMembers = function() {
-    _Tj_Dash_Game_CharacterBase_initMembers.call(this);
-    this.setMoveSpeed(TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed);
+TjKenMate.Param.DashOverhaul.DefaultSettings = TjKenMate.DashOverhaul.DefaultSettings || {};
+TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed = Number(TjKenMate.Parameters['Default Move Speed'] || 5);
+TjKenMate.Param.DashOverhaul.DefaultSettings.defaultDashSpeed = Number(TjKenMate.Parameters['Default Dash Speed'] || 6);
+TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMaxStamina = Number(TjKenMate.Parameters['Default Max Stamina'] || 100);
+TjKenMate.Param.DashOverhaul.DefaultSettings.defaultStaminaCounterLimit = Number(TjKenMate.Parameters['Default Max Stamina'] || 100);
+
+var TjMovementSpeed = 0;
+var TjDashSpeed = 0;
+var TjDashStamina = 0;
+var TjStaminaGainCounter = 0;
+
+
+var _Tj_Dash_Game_Player_initMembers = Game_Player.prototype.initMembers;
+Game_Player.prototype.initMembers = function() {
+    _Tj_Dash_Game_Player_initMembers.call(this);
+    TjMovementSpeed = TjCalcMovementSpeed();
+    TjDashSpeed = TjCalcDashSpeed();
+    TjDashStamina = TjCalcStaminaMax();
 };
 
-var TjDashStamina = 400;
+function TjCalcMovementSpeed(){
+    return TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMoveSpeed;
+};
 
-if (TjKenMate.Param.DisableAlwaysDashOption){
+function TjCalcDashSpeed(){
+    return TjKenMate.Param.DashOverhaul.DefaultSettings.defaultDashSpeed;
+};
+
+function TjCalcStaminaCost(){
+    return 2;
+};
+
+function TjCalcStaminaMax(){
+    return TjKenMate.Param.DashOverhaul.DefaultSettings.defaultMaxStamina;
+};
+
+function TjCalcStaminaLimit(){
+    return TjKenMate.Param.DashOverhaul.DefaultSettings.defaultStaminaCounterLimit;
+};
+
+function TjCalcStaminaGain(){
+    return 1;
+};
+
+function TjcalcStamina(){
+    if(TjStaminaGainCounter > TjCalcStaminaLimit()){
+        TjStaminaGainCounter = 0;
+        return TjDashStamina + TjCalcStaminaGain();
+    };
+    return TjDashStamina;
+};
+
+/*
+if (TjKenMate.Param.DashOverhaul.DisableAlwaysDashOption){
     if(TjKenMate.OptionMenu){}
     else{
         Window_Options.prototype.addGeneralOptions = function() {
@@ -54,13 +113,37 @@ if (TjKenMate.Param.DisableAlwaysDashOption){
     }
 };
 
-var _Tj_DashOverhaul_is_Dash_Button_Pressed = Game_Player.prototype.isDashButtonPressed;
+var _Tj_Dash_Game_Player_isDashButtonPressed = Game_Player.prototype.isDashButtonPressed;
 Game_Player.prototype.isDashButtonPressed = function() {
     if(TjDashStamina == 0)
         return false;
     else if(Input.isPressed('shift')) {
-        TjDashStamina--;
+        //TjDashStamina--;
         console.log(TjDashStamina);
+        return true;
     }
-    _Tj_DashOverhaul_is_Dash_Button_Pressed.call(this);
+    _Tj_Dash_Game_Player_isDashButtonPressed.call(this);
+};
+
+var _Tj_Dash_Game_Player_isDashing = Game_Player.prototype.isDashButtonPressed;
+*/
+
+Game_CharacterBase.prototype.realMoveSpeed = function() {
+    return (this.isDashing() ? TjDashSpeed : TjMovementSpeed);
+};
+
+var _Tj_Dash_Game_Player_increaseSteps = Game_Player.prototype.increaseSteps;
+Game_Player.prototype.increaseSteps = function() {
+    console.log(TjDashStamina);
+    if(TjDashStamina <= 0){
+        this._dashing = false;
+    }
+    if(this._dashing == true){
+        TjDashStamina = TjDashStamina - TjCalcStaminaCost();
+    } else if (TjDashStamina < TjCalcStaminaMax()){
+        TjStaminaGainCounter++;
+        console.log(TjStaminaGainCounter);
+        TjDashStamina = TjcalcStamina();
+    }
+    _Tj_Dash_Game_Player_increaseSteps.call(this);
 };
